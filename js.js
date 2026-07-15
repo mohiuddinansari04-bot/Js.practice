a715af215282ed23bb27f9c897bd56febd60783f
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-07-15 15:56:19 +0530

    main

---
+++ js.js
@@ -0,0 +1,116 @@
+// console.log(typeof(NaN));
+// console.log({}+[])
+
+// function abs(){
+//   return 
+//   {
+//     a: 1
+//   }
+// }
+// console.log(abc());
+
+// let arr =[10,15,30];
+// arr[5] = 100;
+// console.log(arr.length);
+
+// let arr =[15,10,30,40,5,10,30];
+// let uniqueArr =arr.filter((item,index)=>{
+//   return arr.indexOf(item)===index;
+// })
+// console.log(uniqueArr);
+
+
+// function fn(){
+//   return
+//   {name:'abc'};
+// }
+// console.log(fn());
+// const arr = [10,12,18,6,4];
+// function a(b){
+//   return(c)=>{
+//     return b+c;
+//   }
+// }
+// console.log(a(10)(20));
+// console.log("farhan" *"ansari");
+// const str1 =  "farhan";
+// const str2 =  "ansari";
+
+// function abc(str1,str2){
+//   let newstr = "";
+//   let min =Math.min(str1.length,str2.length);
+//   for(let i=0; i<min;i++){
+//     newstr += str1[i]+str2[i];
+
+//   }
+//   return newstr+str1.slice(min)+str2.slice(min);
+// }
+// console.log(abc(str1,str2));
+
+// let obj =[{name:"farhan",marks:65},{name:"ansari",marks:80},{name:"farhan",marks:100}];
+// const newobj =obj.filter((item)=>item.marks>70);
+// console.log(newobj);
+
+// let obj =[{name:"farhan",marks:65},{name:"ansari",marks:80},{name:"farhan",marks:100}  ];
+//  const newobj = obj.filter((item)=>item.marks>40)
+//  console.log(newobj);
+
+// console.log({}=={});
+// console.log({}==={});
+
+
+// var a={
+//   id:1,
+//   title:"meet",
+//   abc:"A",
+// }
+// var b= {
+//   id:2,
+//   title:"ansari"
+// }
+
+// var c= {
+//   ...a,...b
+// }
+// console.log(c);
+
+// const original = {
+//   name: "Farhan",
+//   address: {
+//     city: "Aurangabad"
+//   }
+// };
+
+// const copy = { ...original };
+
+// copy.name = "Ahmed";
+// copy.address.city = "Pune";
+
+// console.log(original);
+// console.log(copy);
+
+
+
+
+// const arr=[1,2,3,4,5];
+// //  const double =arr.map(num=>num*2);
+// //  console.log(double);
+
+// arr.forEach(num=>{
+//   console.log(num*2);
+// });
+
+// const fruits = ["apple", "banana", "cherry"];
+// for(const fruit of fruits ){
+//   console.log(fruit);
+// }
+
+// const person ={
+//   name:"farhan",
+//   age:25,
+//   city:"aurangabad"
+// }
+// for(const key in person){
+//   console.log(key,person[key]);
+// }
+
