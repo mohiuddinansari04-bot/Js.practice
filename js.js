dd5ba6a01fecb398a9483debbc146561a3eb2f4a
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-07-29 12:52:38 +0530

    practice js

--- js.js
+++ js.js
@@ -477,5 +477,14 @@
 
 //remove duplicate
 
-
-
+function removeduplicate(arr){
+    
+    let result=[];
+    for(let i=0;i<arr.length;i++){
+        if(!result.includes(arr[i])){
+            result.push(arr[i])
+        }
+    }
+    return result;
+}
+console.log(removeduplicate([1,2,1,3,4,2,5,3,6,3,7,6]))
