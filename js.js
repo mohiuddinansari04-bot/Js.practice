0e407f5f57b17cf231fc6b063b90689ff7c67862
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-08-04 01:31:46 +0530

    practice

--- js.js
+++ js.js
@@ -668,4 +668,15 @@
 // }
 // console.log(largest([10,20,30,40,50]))
 
-func
+// function removeduplicate(arr){
+//   let result=[];
+//   for(let i=0;i<arr.length;i++){
+//     if(!result.includes(arr[i])){
+//       result.push(arr[i])
+//     }
+    
+
+// }
+//  return result; 
+// }
+// console.log(removeduplicate([1,2,3,4,4,5,5,1]))
