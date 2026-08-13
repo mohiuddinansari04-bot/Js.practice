3507592f76e44ac03baf633b603819a7858e483b
Mohiuddin ansari <mohiuddinansari04@gmail.com>
2026-08-13 16:58:41 +0530

    practice

--- js.js
+++ js.js
@@ -782,8 +782,17 @@
 
 
 
-const arr = ["a", "b", "c"];
-
-for (let val of arr) {
-  console.log(val);
+function ispalendrom(str){
+  let left =0;
+  let right=str.length -1;
+  while(left<right){
+    if(str[left]!==str[right]){
+      return false;
+    }
+    left++;
+    right--;
+  }
+  return true;
 }
+console.log(ispalendrom("madam"))
+console.log(ispalendrom("farhan"))
